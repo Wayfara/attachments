c871e1f5a1a29cfa4194999042398cd2a9c84e12
lb <lb@github.com>
2025-02-11 22:03:11 +0800

    update

--- genGUID.bat
+++ genGUID.bat
@@ -1,6 +1,7 @@
 @echo off
 for /f %%i in ('powershell -Command "[guid]::NewGuid().ToString()"') do set GUID=%%i
+for /f %%i in ('powershell -Command "Get-Date -Format yyyyMMdd"') do set DATETIME=%%i
 set PREFIX=GUID-
 set SUFFIX=-Shanghai
-echo %PREFIX%%GUID%%SUFFIX%
+echo %PREFIX%%GUID%-%DATETIME%
 pause
